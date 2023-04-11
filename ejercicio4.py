9de00eacfff9f1f3521278069cca0232a5e485a5
Mon-MCS <montse_patito@hotmail.com>
2023-04-11 12:53:42 +0100

    Ejercicios Monserrat Cabrera

--- ejercicio4.py
+++ ejercicio4.py
@@ -1,9 +1,9 @@
+from math import pi
 def areaPerimetro(radio):
     area = round(radio*pi*2,4)
     perimetro = round(pow(radio,2)*pi,4) 
     return area, perimetro
 def ejercicio4():
-    from math import pi
     numero = float(input("Introduzca el radio del círculo: "))
     area , perimetro = areaPerimetro(numero)
     print(f"El área del círculo es {area} y el perímetro el círculo es {perimetro}")
