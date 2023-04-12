787002d5fb0435c3b7a46258c697f7b9ff7b3e93
Kerolink <PVergelCelis@gmail.com>
2023-04-12 13:48:54 +0200

    Actualización del 16 en clase

--- ejercicio16.py
+++ ejercicio16.py
@@ -9,38 +9,40 @@ def ejercicio16():
     loop = True
     self_destruct = True
     # Proceso
-    try:
+
         # Mientras loop sea True preguntaremos al usuario si quiere anadir numeros
-        while loop == True:
-            user_input = input(
-                "Añade un número a la lista, no añada nada para dejar de añadir números: ")
-            # Si no se introduce nada loop cambia a False y se cierra el bucle
-            if user_input == "":
-                loop = False
-            # Si se introduce un numero se asigna al final de la lista de input_list
-            else:
-                input_list.append(float(user_input))
-                # Tambien asignaremos self_destruct False
-                self_destruct = False
+    while loop == True:
+            try:
+                user_input = input(
+                    "Añade un número a la lista, no añada nada para dejar de añadir números: ")
+                # Si no se introduce nada loop cambia a False y se cierra el bucle
+                if user_input == "":
+                    loop = False
+                # Si se introduce un numero se asigna al final de la lista de input_list
+                else:
+                    input_list.append(float(user_input))
+                    # Tambien asignaremos self_destruct False
+                    self_destruct = False
+            # El programa dará error si el usuario introduce letras o espacios en lugar de numeros
+            except:
+                print("Error entrada inválida")
 
         # Para cada valor de la lista,
-        for x in input_list:
+    for x in input_list:
             # Asignamos a SumaTotal la suma de x y SumaTotal
             SumaTotal = SumaTotal+x
             # Asignamos a i la suma de i mas 1, esto indica el numero de valores introducidos
             i = i+1
         # Asignamos a Media la división de Sumatotal entre i
-        Media = SumaTotal/i
+            Media = SumaTotal/i
         # Salida
         # Si al menos se introdujo un valor mostraremos
-        if self_destruct == False:
+    if self_destruct == False:
             print("La lista de numeros que ha introducido es la siguiente: ")
             print(input_list)
             print("El resultado de la suma de los numeros es: ", SumaTotal)
             print("La media es: ", Media)
         # Si el usuario no llegó a introducir ni un solo número, self_destruct seguirá siendo True y mostrará un mensaje
-        else:
-            print("No introdujiste nada :( ")
-    # El programa dará error si el usuario introduce letras o espacios en lugar de numeros
-    except:
-        print("Error")
+    else:
+        print("No introdujiste nada :( ")
+
